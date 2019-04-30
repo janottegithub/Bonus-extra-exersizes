e9e6fc383f5f5177ada390d68fdb4b77a9645389
jan otte <janotte@MBP-van-jan.lan>
2019-04-30 10:20:56 +0200

    3.3-Radom-backgroundColors

--- 03-colors/03-change-bcg-three/script.js
+++ 03-colors/03-change-bcg-three/script.js
@@ -8,9 +8,13 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
-
-(function() {
-
-    // your code here
-
-})();
+var run = document.getElementById("run");
+run.addEventListener("click",code);
+function code() {
+var x = Math.floor(Math.random() * 256);
+var y = Math.floor(Math.random() * 256);
+var z = Math.floor(Math.random() * 256);
+var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+console.log(bgColor);
+document.body.style.background = bgColor;
+};
